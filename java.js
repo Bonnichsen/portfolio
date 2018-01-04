f8c6580430654d7d58096a3ee8ff6e3690eee3af
Benjamin Reinhold Bonnichsen <Benjamin@Benjamin-Reinhold-Bonnichsens-MacBook-Pro.local>
2018-01-04 15:15:30 +0100

    small fixes jQuery, texting for sections, video+pics for Moon Bar section

--- java.js
+++ java.js
@@ -10,6 +10,8 @@ $(document).ready(function () {
         $(".portfolio_text").hide();
         $(".cv_text").hide();
         $(".home_text").show();
+        $(".active").slideUp();
+        $(".project_info").removeClass("active");
     });
 
     $(".nav_cv").click(function () {
@@ -19,6 +21,8 @@ $(document).ready(function () {
         $(".home_text").hide();
         $(".portfolio_text").hide();
         $(".cv_text").show();
+        $(".active").slideUp();
+        $(".project_info").removeClass("active");
     });
 
     $(".nav_portfolio").click(function () {
@@ -27,6 +31,8 @@ $(document).ready(function () {
         $(".home_text").hide();
         $(".cv_text").hide();
         $(".portfolio_text").show();
+        $(".active").slideUp();
+        $(".project_info").removeClass("active");
     });
 
     $(".project").click(function () {
@@ -36,7 +42,7 @@ $(document).ready(function () {
         $(".project_info").addClass("active");
         $("html, body").animate({
             scrollTop: 445
-        }, 800);
+        }, 900);
     });
 
     /*---------- PROJECT INFORMATION ------*/
@@ -45,31 +51,31 @@ $(document).ready(function () {
         $("#info_tommy").slideDown(900);
     });
     $("#cphdox").click(function () {
-        $("#info_cphdox").slideDown("slow");
+        $("#info_cphdox").slideDown(900);
     });
     $("#redesign").click(function () {
-        $("#info_redesign").slideDown("slow");
+        $("#info_redesign").slideDown(900);
     });
     $("#kea_rent").click(function () {
-        $("#info_kea_rent").slideDown("slow");
+        $("#info_kea_rent").slideDown(900);
     });
     $("#reachout").click(function () {
-        $("#info_reachout").slideDown("slow");
+        $("#info_reachout").slideDown(900);
     });
     $("#moonbar_probably").click(function () {
-        $("#info_moonbar_probably").slideDown("slow");
+        $("#info_moonbar_probably").slideDown(900);
     });
     $("#sexogsamfund").click(function () {
-        $("#info_sexogsamfund").slideDown("slow");
+        $("#info_sexogsamfund").slideDown(900);
     });
     $("#stilartsite").click(function () {
-        $("#info_stilartsite").slideDown("slow");
+        $("#info_stilartsite").slideDown(900);
     });
     $("#intro_video").click(function () {
-        $("#info_intro").slideDown("slow");
+        $("#info_intro").slideDown(900);
     });
     $("#animation_kealife").click(function () {
-        $("#info_animation_kealife").slideDown("slow");
+        $("#info_animation_kealife").slideDown(900);
     });
 
     /*---------- RETURN ARROW ---------*/
